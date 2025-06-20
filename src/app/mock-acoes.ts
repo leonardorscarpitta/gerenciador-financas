22d86d8fa44cc2cd97142a750ec353eac46518d0
Leonardo <leonardorscarpitta@gmail.com>
2025-06-20 11:02:52 -0300

    feat: create new attributes

--- src/app/mock-acoes.ts
+++ src/app/mock-acoes.ts
@@ -3,6 +3,8 @@ export interface STOCK {
   valorAtual: number;
   valorCompra: number;
   quantidade: number;
+  timestamp: string,
+  tipoAcao: string
 }
 
 export const MOCK_STOCKS: STOCK[] = [
@@ -11,29 +13,7 @@ export const MOCK_STOCKS: STOCK[] = [
     valorAtual: 0,
     valorCompra: 30.0,
     quantidade: 100,
-  },
-  {
-    nome: "VALE3",
-    valorAtual: 0,
-    valorCompra: 55.0,
-    quantidade: 50,
-  },
-  {
-    nome: "AAPL",
-    valorAtual: 0,
-    valorCompra: 180.0,
-    quantidade: 20,
-  },
-  {
-    nome: "ITUB4",
-    valorAtual: 0,
-    valorCompra: 25.0,
-    quantidade: 80,
-  },
-  {
-    nome: "AMZN",
-    valorAtual: 0,
-    valorCompra: 3100.0,
-    quantidade: 5,
-  },
+    timestamp: "25/07/2005 - 20:59",
+    tipoAcao: "",
+  }
 ];
