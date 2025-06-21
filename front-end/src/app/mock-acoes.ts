25bf341efbb03298dc81fbc7757217987bea464e
Leonardo <leonardorscarpitta@gmail.com>
2025-06-21 19:16:54 -0300

    chore: remove default mock

--- front-end/src/app/mock-acoes.ts
+++ front-end/src/app/mock-acoes.ts
@@ -6,14 +6,3 @@ export interface STOCK {
   timestamp: string,
   tipoAcao: string
 }
-
-export const MOCK_STOCKS: STOCK[] = [
-  {
-    nome: "PETR4",
-    valorAtual: 0,
-    valorCompra: 30.0,
-    quantidade: 100,
-    timestamp: "25/07/2005 - 20:59",
-    tipoAcao: "",
-  }
-];
